4ebb9d815a1d2a3625763bc28cdd45b220a9a669
Douglass Turner <douglass.turner@gmail.com>
2022-03-09 16:14:21 -0500

    Configuration now supports resolution and normalization (#6)
    * notebook-ing
    
    * Configuration now supports resolution and normalization

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(name='juicebox',
                  packages=['juicebox'],
-                 version='0.1.0',
+                 version='0.1.1',
                  description='Package for embedding the juicebox.js hic visualization in IPython notebooks',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
